5b80629cef6d0c903ab4ab2e27b6db3015a68ce5
Boaventura <scavenger.gnu@gmail.com>
2020-08-08 14:16:47 -0300

    :bug: fixing try-catch block in genres load method. #22

--- android/src/main/java/boaventura/com/devel/br/flutteraudioquery/loaders/GenreLoader.java
+++ android/src/main/java/boaventura/com/devel/br/flutteraudioquery/loaders/GenreLoader.java
@@ -108,16 +108,15 @@ public class GenreLoader extends AbstractLoader {
                                                      String sortOrder) {
 
             List<Map<String, Object>> dataList= new ArrayList<>();
-            Cursor genreCursor = m_resolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                    new String[] {"Distinct " + GENRE_PROJECTION[0] }, selection,
-                    selectionArgs, sortOrder);
+            Cursor genreCursor = null;
 
-//            Cursor genreCursor = m_resolver.query(MediaStore.Audio.Genres.EXTERNAL_CONTENT_URI,
-//                    GENRE_PROJECTION, selection, selectionArgs, sortOrder);
+            try {
+                genreCursor = m_resolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                        new String[]{"Distinct " + GENRE_PROJECTION[0]}, selection,
+                        selectionArgs, sortOrder);
 
-            if (genreCursor != null){
-                while ( genreCursor.moveToNext() ){
-                    try {
+                if (genreCursor != null) {
+                    while (genreCursor.moveToNext()) {
                         Map<String, Object> data = new HashMap<>();
                         for (String column : genreCursor.getColumnNames()) {
                             String genreName = genreCursor.getString(
@@ -126,13 +125,13 @@ public class GenreLoader extends AbstractLoader {
                         }
                         dataList.add(data);
                     }
-
-                    catch(Exception ex){
-                        Log.e(TAG_ERROR, "GenreLoader::loadData method exception");
-                        Log.e(TAG_ERROR, ex.getMessage() );
-                    }
+                    genreCursor.close();
                 }
-                genreCursor.close();
+            }
+
+            catch(RuntimeException ex){
+                Log.e(TAG_ERROR, "GenreLoader::loadData method exception");
+                //Log.e(TAG_ERROR, ex.getMessage() );
             }
 
             return dataList;
