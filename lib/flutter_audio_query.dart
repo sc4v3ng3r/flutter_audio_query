51887094ab60adb0d34206ae751c9c7895c6df92
Boaventura <scavenger.gnu@gmail.com>
2021-12-16 21:08:51 -0300

    :recycle: refactoring models

--- lib/flutter_audio_query.dart
+++ lib/flutter_audio_query.dart
@@ -24,19 +24,18 @@ library flutter_audio_query;
 
 import 'dart:typed_data';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 
-part 'src/album_info.dart';
-part 'src/album_sort_type.dart';
-part 'src/artist_info.dart';
-part 'src/artist_sort_type.dart';
-part 'src/data_model.dart';
+part 'src/models/album/album_info.dart';
+part 'src/models/album/album_sort_type.dart';
+part 'src/models/artist/artist_info.dart';
+part 'src/models/artist/artist_sort_type.dart';
+part 'src/models/data_model.dart';
 part 'src/flutter_audio_query.dart';
-part 'src/genre_info.dart';
-part 'src/genre_sort_type.dart';
-part 'src/playlist_info.dart';
-part 'src/playlist_sort_type.dart';
-part 'src/song_info.dart';
-part 'src/song_sort_type.dart';
+part 'src/models/genre/genre_info.dart';
+part 'src/models/genre/genre_sort_type.dart';
+part 'src/models/playlist/playlist_info.dart';
+part 'src/models/playlist/playlist_sort_type.dart';
+part 'src/models/song/song_info.dart';
+part 'src/models/song/song_sort_type.dart';
